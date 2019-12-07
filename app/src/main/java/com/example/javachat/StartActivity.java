ce780b8e78fad563fce909b5fc25a954f6cf1edd
Volkan kahraman <khrmnvolkan@gmail.com>
2019-12-08 01:40:56 +0300

    Chat özellikleri implimente edildi

--- app/src/main/java/com/example/javachat/StartActivity.java
+++ app/src/main/java/com/example/javachat/StartActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -35,7 +36,6 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
 
 
         login = findViewById(R.id.login);
