b62b9e8e35c04034e84c5ae51214241b1b7f7670
Volkan kahraman <khrmnvolkan@gmail.com>
2019-12-07 18:10:17 +0300

    Üyelik ve Giriş ekranları yapıldı

---
+++ app/src/main/java/com/example/javachat/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.javachat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Giriş");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
